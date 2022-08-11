6a4adf745254348a544e49b089f3ef417e8f47dd
Blair Lyons <blair208@gmail.com>
2022-08-11 13:36:04 -0700

    try downloading one test file

--- examples/post_processing/docker/post_processing.sh
+++ examples/post_processing/docker/post_processing.sh
@@ -10,8 +10,9 @@ case ${SIMULATION_TYPE} in
 	;;
 esac
 S3_FILE_PATH="${S3_INPUT_URL}outputs/"
+echo $S3_FILE_PATH
 FILE_NAME="file"
-aws s3 cp . $S3_FILE_PATH --exclude "*" --include "${PARAM_SET_NAME}*.h5" --recursive --dryrun
+aws s3 cp . $S3_FILE_PATH --exclude "*" --include "actin_twist_bend_dihedral_strength10_tangent_free_9.h5" --recursive --dryrun
 
 # Post-process files
 python post_processing.py $FILE_NAME
