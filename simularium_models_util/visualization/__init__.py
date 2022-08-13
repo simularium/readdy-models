e8e07d0942a5bf7d707e24f1a13576bbe72c25ee
Blair Lyons <blair208@gmail.com>
2022-08-12 18:17:23 -0700

    export ACTIN_DISPLAY_DATA

--- simularium_models_util/visualization/__init__.py
+++ simularium_models_util/visualization/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from .actin_visualization import ActinVisualization  # noqa: F401
+from .actin_visualization import ActinVisualization, ACTIN_DISPLAY_DATA  # noqa: F401
 from .kinesin_visualization import KinesinVisualization  # noqa: F401
 from .microtubules_visualization import MicrotubulesVisualization  # noqa: F401
