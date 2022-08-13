707ef3d45f7fa36a5400a1e0ffd1d96a0313cd95
Blair Lyons <blair208@gmail.com>
2022-08-12 22:29:42 -0700

    Update post_processing.py

--- examples/actin/post_processing.py
+++ examples/actin/post_processing.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 import os
-import argparse
 import boto3
 
 from visualize_actin import visualize_actin
@@ -10,45 +9,42 @@ from visualize_actin import visualize_actin
 
 S3_BUCKET_NAME = "readdy-working-bucket"
 
-REPLICATES = 10
+# EXPERIMENT_NAME = "actin_twist_bend_2"
+EXPERIMENT_NAME = "test"
+
+REPLICATES = 5
 
 EXPERIMENT_CONDITIONS = [
-    "1_radial_fixed",  # no longitudinal bonds
-    "1_radial_free",
-    "1_tangent_fixed",
-    "1_tangent_free",
-    "dihedral_strength10_radial_fixed",  # longitudinal bonds
-    "dihedral_strength10_radial_free",
-    "dihedral_strength10_tangent_fixed",
-    "dihedral_strength10_tangent_free",
+    "no-long_tangent_fixed",
+    "no-long_tangent_free",
+    "no-long_radial_fixed",
+    "no-long_radial_free",
+    "long_tangent_fixed",
+    "long_tangent_free",
+    "long_radial_fixed",
+    "long_radial_free",
 ]
 
-
-def get_output_name(condition):
-    if "dihedral_strength" in condition:
-        return "long_" + condition[20:]
-    else:
-        return "no-long_" + condition[2:]
 
 def main():
     s3 = boto3.client('s3')
     for condition_index in range(len(EXPERIMENT_CONDITIONS)):
         condition = EXPERIMENT_CONDITIONS[int(condition_index)]
-        output_name = get_output_name(condition)
-        print(f"Re-processing {output_name}")
+        print(f"Post-processing {condition}")
         # download
         if not os.path.exists("h5_files/"):
+            print("Downloading from S3...")
             os.mkdir("h5_files/")
             for rep in range(REPLICATES):
-                input_file_name = f"outputs/actin_twist_bend_{condition}_{rep}.h5"
-                output_file_name = f"actin_twist_bend_{output_name}_{rep}.h5"
-                s3.download_file(S3_BUCKET_NAME, input_file_name, f"h5_files/{output_file_name}")
+                file_name = f"{EXPERIMENT_NAME}_{condition}_{rep}"
+                s3.download_file(S3_BUCKET_NAME, f"outputs/{file_name}.h5", f"h5_files/{file_name}.h5")
+                s3.download_file(S3_BUCKET_NAME, f"outputs/{file_name}.dat", f"h5_files/{file_name}.dat")
         # visualize
         visualize_actin(
             dir_path="h5_files/",
-            box_size=200,
-            total_steps=4e6,
-            experiment_name=f"actin_twist_bend_{output_name}_",
+            box_size=600,
+            total_steps=1e5,
+            experiment_name=f"{EXPERIMENT_NAME}_",
             periodic_boundary=False,
             plot_bend_twist=True,
             plot_polymerization=False,
