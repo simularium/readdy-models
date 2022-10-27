f6049b473af125209b6e3b675340f0e445a4146a
Blair Lyons <blair208@gmail.com>
2022-10-27 13:09:25 -0700

    WIP test for missing one crosslink

--- simularium_models_util/tests/microtubules/test_microtubules_attach.py
+++ simularium_models_util/tests/microtubules/test_microtubules_attach.py
@@ -58,6 +58,19 @@ def tubulin_has_sites(ring_ix, filament_ix, ring_connections):
     )
 
 
+def get_edge(pid1, pid2):
+    """
+    Create a tuple of particle ids for the edge
+    
+    ReaDDy seems to sometimes not add edges if the first int 
+    in the tuple is greater than the second. Why???
+    """
+    if pid1 < pid2:
+        return (pid1, pid2)
+    else:
+        return (pid2, pid1)
+
+
 def add_init_state(readdy_simulation, ring_connections, topology_type):
     """
     Create patch of tubulins with given types and connections
@@ -89,26 +102,26 @@ def add_init_state(readdy_simulation, ring_connections, topology_type):
             # bond to minus end tubulin
             if ring_ix > 0:
                 minus_id = filament_ix * n_rings + ring_ix - 1
-                edges.append((tubulin_id, minus_id))
+                edges.append(get_edge(tubulin_id, minus_id))
             # bond to plus end tubulin
             if ring_ix < n_rings - 1:
                 plus_id = filament_ix * n_rings + ring_ix + 1
-                edges.append((tubulin_id, plus_id))
+                edges.append(get_edge(tubulin_id, plus_id))
             # bond to prev ring tubulin
             if filament_ix > 0 and ring_connections[ring_ix + 1, filament_ix]:
                 prev_id = (filament_ix - 1) * n_rings + ring_ix
-                edges.append((tubulin_id, prev_id))
+                edges.append(get_edge(tubulin_id, prev_id))
             # bond to next ring tubulin
             if filament_ix < n_filaments - 1 and ring_connections[ring_ix + 1, filament_ix + 1]:
                 next_id = (filament_ix + 1) * n_rings + ring_ix
-                edges.append((tubulin_id, next_id))
+                edges.append(get_edge(tubulin_id, next_id))
             # sites
             if has_sites:
                 tublin_position = positions[len(positions) - 1]
                 site_out_id = n_tubulin + len(site_types)
                 for site_ix in range(5):
                     site_id = site_out_id + site_ix
-                    edges.append((site_id, tubulin_id))  # edge to tubulin
+                    edges.append(get_edge(site_id, tubulin_id))  # edge to tubulin
                     if site_ix == 0:
                         d_site = np.array([0., 2., 0.])
                         site_types.append("site#out")
@@ -116,23 +129,23 @@ def add_init_state(readdy_simulation, ring_connections, topology_type):
                         d_site = 0.5 * (d_ring if site_ix > 2 else d_filament)
                         d_site *= -1 if site_ix % 2 == 1 else 1
                         site_types.append(f"site#{site_ix}")
-                        edges.append((site_id, site_out_id))  # edge to site#out
-                        if site_ix > 2:  # diagonal edges between sites
-                            edges.append((site_id, site_id - (site_ix - 2))) 
-                            edges.append((site_id, site_id - (site_ix - 1)))
+                        edges.append(get_edge(site_id, site_out_id))  # edge to site#out
+                        # if site_ix > 2:  # diagonal edges between sites
+                        #     edges.append((site_id, site_id - (site_ix - 2))) 
+                        #     edges.append((site_id, site_id - (site_ix - 1)))
                     site_positions.append(tublin_position + d_site)
-                # site bonds to minus end tubulin sites
-                if ring_ix > 0 and tubulin_has_sites(ring_ix - 1, filament_ix, ring_connections):
-                    minus_site_out_id = site_out_id - 5
-                    edges.append((site_out_id + 1, minus_site_out_id + 1))  # sites 1
-                    edges.append((site_out_id + 2, minus_site_out_id + 2))  # sites 2
-                    edges.append((site_out_id + 3, minus_site_out_id + 4))  # sites 3 and 4
-                # site bonds to plus end tubulin sites
-                if ring_ix < n_rings - 1 and tubulin_has_sites(ring_ix + 1, filament_ix, ring_connections):
-                    plus_site_out_id = site_out_id + 5
-                    edges.append((site_out_id + 1, plus_site_out_id + 1))  # sites 1
-                    edges.append((site_out_id + 2, plus_site_out_id + 2))  # sites 2
-                    edges.append((site_out_id + 4, plus_site_out_id + 3))  # sites 3 and 4
+                # # site bonds to minus end tubulin sites
+                # if ring_ix > 0 and tubulin_has_sites(ring_ix - 1, filament_ix, ring_connections):
+                #     minus_site_out_id = site_out_id - 5
+                #     edges.append((site_out_id + 1, minus_site_out_id + 1))  # sites 1
+                #     edges.append((site_out_id + 2, minus_site_out_id + 2))  # sites 2
+                #     edges.append((site_out_id + 3, minus_site_out_id + 4))  # sites 3 and 4
+                # # site bonds to plus end tubulin sites
+                # if ring_ix < n_rings - 1 and tubulin_has_sites(ring_ix + 1, filament_ix, ring_connections):
+                #     plus_site_out_id = site_out_id + 5
+                #     edges.append((site_out_id + 1, plus_site_out_id + 1))  # sites 1
+                #     edges.append((site_out_id + 2, plus_site_out_id + 2))  # sites 2
+                #     edges.append((site_out_id + 4, plus_site_out_id + 3))  # sites 3 and 4
     microtubule = readdy_simulation.add_topology(
         topology_type, types + site_types, np.array(positions + site_positions)
     )
@@ -154,7 +167,7 @@ def run_readdy(mt_simulation):
     timestep = 0.1
     readdy_actions = mt_simulation.simulation._actions
     init = readdy_actions.initialize_kernel()
-    diffuse = readdy_actions.integrator_euler_brownian_dynamics(timestep)
+    # diffuse = readdy_actions.integrator_euler_brownian_dynamics(timestep)
     calculate_forces = readdy_actions.calculate_forces()
     create_nl = readdy_actions.create_neighbor_list(mt_simulation.system.calculate_max_cutoff().magnitude)
     update_nl = readdy_actions.update_neighbor_list()
@@ -176,8 +189,8 @@ def run_readdy(mt_simulation):
 def state_to_str(monomers):
     result = "topologies:\n"
     for top_id in monomers["topologies"]:
-        top_name = monomers["topologies"][top_id]["type_name"]
-        result += f"  {top_id} : {top_name}\n"
+        top = monomers["topologies"][top_id]
+        result += f"  {top_id} : {top}\n"
     result += "particles:\n"
     for particle_id in monomers["particles"]:
         particle = monomers["particles"][particle_id]
@@ -189,13 +202,59 @@ def state_to_str(monomers):
     
 def check_readdy_state(mt_simulation, expected_monomers):
     test_monomers = ReaddyUtil.get_current_monomers(mt_simulation.simulation.current_topologies)
+    raise Exception(state_to_str(test_monomers))
     assert_monomers_equal(test_monomers, expected_monomers, test_position=False)
 
 
 @pytest.mark.parametrize(
     "ring_connections, topology_type, expected_monomers",
     [
-        # 5 rings x 4 filaments
+        # 5 rings x 4 filaments, fully crosslinked
+        # (
+        #     # ring connections: + 2 rings, + 1 filament edges
+        #     np.array([
+        #     # f       0     1     2     3           r
+        #         [True, True, True, True, True], 
+        #         [True, True, True, True, True],   # 0
+        #         [True, True, True, True, True],   # 1
+        #         [True, True, True, True, True],   # 2
+        #         [True, True, True, True, True],   # 3
+        #         [True, True, True, True, True],   # 4
+        #         [True, True, True, True, True], 
+        #     ]),
+        #     "Microtubule",
+        #     {
+        #         "topologies": {
+        #             0: {
+        #                 "type_name": "Microtubule",
+        #                 "particle_ids": range(20),
+        #             }
+        #         },
+        #         "particles": {
+        #             0: {"unique_id": 0, "type_name": "tubulinA#GTP_1_1", "position": np.zeros(3), "neighbor_ids": [1, 5]},
+        #             1: {"unique_id": 1, "type_name": "tubulinB#GTP_2_1", "position": np.zeros(3), "neighbor_ids": [0, 2, 6]},
+        #             2: {"unique_id": 2, "type_name": "tubulinA#GTP_3_1", "position": np.zeros(3), "neighbor_ids": [1, 3, 7]},
+        #             3: {"unique_id": 3, "type_name": "tubulinB#GTP_1_2", "position": np.zeros(3), "neighbor_ids": [2, 4, 8]},
+        #             4: {"unique_id": 4, "type_name": "tubulinA#GTP_2_2", "position": np.zeros(3), "neighbor_ids": [3, 9]},
+        #             5: {"unique_id": 5, "type_name": "tubulinA#GTP_1_2", "position": np.zeros(3), "neighbor_ids": [6, 0, 10]},
+        #             6: {"unique_id": 6, "type_name": "tubulinB#GTP_2_2", "position": np.zeros(3), "neighbor_ids": [5, 7, 1, 11]},
+        #             7: {"unique_id": 7, "type_name": "tubulinA#GTP_3_2", "position": np.zeros(3), "neighbor_ids": [6, 8, 2, 12]},
+        #             8: {"unique_id": 8, "type_name": "tubulinB#GTP_1_3", "position": np.zeros(3), "neighbor_ids": [7, 9, 3, 13]},
+        #             9: {"unique_id": 9, "type_name": "tubulinA#GTP_2_3", "position": np.zeros(3), "neighbor_ids": [8, 4, 14]},
+        #             10: {"unique_id": 10, "type_name": "tubulinA#GTP_1_3", "position": np.zeros(3), "neighbor_ids": [11, 5, 15]},
+        #             11: {"unique_id": 11, "type_name": "tubulinB#GTP_2_3", "position": np.zeros(3), "neighbor_ids": [10, 12, 6, 16]},
+        #             12: {"unique_id": 12, "type_name": "tubulinA#GTP_3_3", "position": np.zeros(3), "neighbor_ids": [11, 13, 7, 17]},
+        #             13: {"unique_id": 13, "type_name": "tubulinB#GTP_1_1", "position": np.zeros(3), "neighbor_ids": [12, 14, 8, 18]},
+        #             14: {"unique_id": 14, "type_name": "tubulinA#GTP_2_1", "position": np.zeros(3), "neighbor_ids": [13, 9, 19]},
+        #             15: {"unique_id": 15, "type_name": "tubulinA#GTP_1_1", "position": np.zeros(3), "neighbor_ids": [16, 10]},
+        #             16: {"unique_id": 16, "type_name": "tubulinB#GTP_2_1", "position": np.zeros(3), "neighbor_ids": [15, 17, 11]},
+        #             17: {"unique_id": 17, "type_name": "tubulinA#GTP_3_1", "position": np.zeros(3), "neighbor_ids": [16, 18, 12]},
+        #             18: {"unique_id": 18, "type_name": "tubulinB#GTP_1_2", "position": np.zeros(3), "neighbor_ids": [17, 19, 13]},
+        #             19: {"unique_id": 19, "type_name": "tubulinA#GTP_2_2", "position": np.zeros(3), "neighbor_ids": [18, 14]},
+        #         },
+        #     }
+        # ),
+        # 5 rings x 4 filaments, missing one ring edge
         (
             # ring connections: + 2 rings, + 1 filament edges
             np.array([
@@ -203,7 +262,7 @@ def check_readdy_state(mt_simulation, expected_monomers):
                 [True, True, True, True, True], 
                 [True, True, True, True, True],   # 0
                 [True, True, True, True, True],   # 1
-                [True, True, True, True, True],   # 2
+                [True, True, False, True, True],  # 2
                 [True, True, True, True, True],   # 3
                 [True, True, True, True, True],   # 4
                 [True, True, True, True, True], 
@@ -246,3 +305,4 @@ def test_attach(ring_connections, topology_type, expected_monomers):
     mt_simulation = create_simulation(ring_connections, topology_type)
     run_readdy(mt_simulation)
     check_readdy_state(mt_simulation, expected_monomers)
+    del mt_simulation
